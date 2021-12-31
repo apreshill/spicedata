cbcc775647413687806d40f34a4ea915bfc2194d
Alison Presmanes Hill <apreshill@gmail.com>
2021-12-31 14:57:42 -0800

    document the dataset

---
+++ R/studio_album_tracks.R
@@ -0,0 +1,12 @@
+#' Spice Girls Studio Album Tracks
+#'
+#' Audio features of each song from the three studio albums by the Spice Girls. From Spotify.
+#'
+#' @format A tibble with 31 rows and 25 variables:
+#' \describe{
+#'   \item{artist_name}{chr Name of the artist}
+#'   \item{artist_id}{chr Unique ID number of artist}
+#' }
+#' @source <https://github.com/rfordatascience/tidytuesday/tree/master/data/2021/2021-12-14>
+"studio_album_tracks"
+
